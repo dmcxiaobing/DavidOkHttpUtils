80e90ccb777c74310347ee0a26ced2b9b01ca90b
Administrator <admin>
2016-09-25 13:45:09 +0800

    抽取API常量，以及添加gson解析

---
+++ app/src/main/java/davidokhttputils/qq986945193/com/davidokhttputils/constants/Constant.java
@@ -0,0 +1,17 @@
+package davidokhttputils.qq986945193.com.davidokhttputils.constants;
+/**
+ * @author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @交流Qq ：986945193
+ */
+
+/**
+ * 常量值
+ */
+public class Constant {
+    public static final String API_KEY = "apikey";
+    public static final String API_KEY_SECRET = "";
+
+}
